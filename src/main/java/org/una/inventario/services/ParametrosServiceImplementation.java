9248b546f45d1dcc9ad4e1d1991812dd9a2b2992
Maito1794 <isaacmmorab@gmail.com>
2021-10-26 19:59:27 -0600

    Update ParametrosServiceImplementation.java

--- src/main/java/org/una/inventario/services/ParametrosServiceImplementation.java
+++ src/main/java/org/una/inventario/services/ParametrosServiceImplementation.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.una.inventario.dto.ParametrosDTO;
-import org.una.inventario.dto.UsuarioDTO;
 import org.una.inventario.entities.Parametros;
 import org.una.inventario.exceptions.NotFoundInformationException;
 import org.una.inventario.repositories.IParametrosRepository;
